d19c29ed8d0db4b6230e672f314252b711305dbb
Biscuit46 <m18890085125@163.com>
2019-02-27 21:40:47 +0800

    first commit

---
+++ BZOJ/1001.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<algorithm>
+#include<queue>
+#include<set>
+#include<map>
+#include<iostream>
+using namespace std;
+#define ll long long
+#define re register
+#define file(a) freopen(a".in","r",stdin);freopen(a".out","w",stdout)
+inline int gi()
+{
+	int f=1,sum=0;char ch=getchar();
+	while(ch>'9' || ch<'0'){if(ch=='-')f=-1;ch=getchar();}
+	while(ch>='0' && ch<='9'){sum=(sum<<3)+(sum<<1)+ch-'0';ch=getchar();}
+	return f*sum;
+}
+int main()
+{
+  
+	return 0;
+}
